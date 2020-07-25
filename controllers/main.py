16ff9205bda1d35cb75206e1c014475aa2c2b489
johnny <vmjuan90@gmail.com>
2020-07-26 00:08:23 +0200

    Test

--- controllers/main.py
+++ controllers/main.py
@@ -13,11 +13,48 @@ class WebsiteSaleGuadalstoreProductSeo(WebsiteSale):
 
     @http.route()
     def product(self, product, category='', search='', **kwargs):
-        """ Added ref and barcode to original response """
-        res = super(WebsiteSaleGuadalstoreProductSeo, self).product(product, category, search, **kwargs)
+        if not product.can_access_from_current_website():
+            raise NotFound()
 
-        res.update({
-            seoldjson: "{'name': 'El nombre'}"
-        })
+        product_context = dict(request.env.context,
+                               active_id=product.id,
+                               partner=request.env.user.partner_id)
+        ProductCategory = request.env['product.public.category']
 
-        return res
+        if category:
+            category = ProductCategory.browse(int(category)).exists()
+
+        attrib_list = request.httprequest.args.getlist('attrib')
+        attrib_values = [[int(x) for x in v.split("-")] for v in attrib_list if v]
+        attrib_set = {v[1] for v in attrib_values}
+
+        keep = QueryURL('/shop', category=category and category.id, search=search, attrib=attrib_list)
+
+        categs = ProductCategory.search([('parent_id', '=', False)])
+
+        pricelist = request.website.get_current_pricelist()
+
+        from_currency = request.env.user.company_id.currency_id
+        to_currency = pricelist.currency_id
+        compute_currency = lambda price: from_currency._convert(price, to_currency, request.env.user.company_id, fields.Date.today())
+
+        if not product_context.get('pricelist'):
+            product_context['pricelist'] = pricelist.id
+            product = product.with_context(product_context)
+
+        values = {
+            'search': search,
+            'category': category,
+            'pricelist': pricelist,
+            'attrib_values': attrib_values,
+            'compute_currency': compute_currency,
+            'attrib_set': attrib_set,
+            'keep': keep,
+            'categories': categs,
+            'main_object': product,
+            'product': product,
+            'optional_product_ids': [p.with_context({'active_id': p.id}) for p in product.optional_product_ids],
+            'get_attribute_exclusions': self._get_attribute_exclusions,
+            'seoldjson': "{'name': 'casa'}"
+        }
+        return request.render("website_sale.product", values)
